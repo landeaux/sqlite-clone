5a52592a43b6ce5ad74471c3ce3bf2acb2058c1e
landeaux <pluckedowl@gmail.com>
2020-02-05 08:32:30 -0800

    Get createDatabase() working

--- sqlite-clone.py
+++ sqlite-clone.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
+import os
+
 quitting = False
 
 def parseInput (input):
     print('parseInput() called with input: ', input)
 
 def createDatabase (dbName):
-    print('createDatabase() called with dbName:', dbName)
+    print('createDatabase() called with dbName: ', dbName)
+    parentPath = os.getcwd()
+    directory = dbName
+    fullPath = os.path.join(parentPath, directory)
+    os.mkdir(fullPath)
+    print('database created at ', fullPath)
 
 while not quitting:
     userInput = raw_input('> ')
