5824ba54624af1d770bf0b13787821555ce76e92
landeaux <pluckedowl@gmail.com>
2020-02-05 23:15:56 -0800

    add doc comments and inline comments

--- sqlite-clone.py
+++ sqlite-clone.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
-import os
-import re
+# Programming Assignment 1 - Metadata Management
+# Author: Adam Landis
+
+import os # for writing directories and files
+import re # for using regular expressions
 
 def parseInput (input):
     print('parseInput() called with input: ', input)
 
 def createDatabase (dbName):
+    """
+    Creates a database with the given name
+
+    dbName -- the name of the database to create
+    """
     parentPath = os.getcwd()
     directory = dbName
     fullPath = os.path.join(parentPath, directory)
@@ -17,12 +25,23 @@ def createDatabase (dbName):
         print('!Failed to create database %s because it already exists.' % dbName)
 
 def exitProgram ():
+    """
+    Exits the program
+    """
     quit()
 
 def printHelp ():
+    """
+    Prints all of the allowed dot-commands and their function
+    """
     print('.exit                  Exit this program')
 
 def create (queryString):
+    """
+    Initiates a CREATE command
+
+    queryString -- the remaining query after the CREATE keyword
+    """
     resourceTypes = {
         'DATABASE': createDatabase
     }
@@ -43,38 +62,48 @@ queryCommands = {
     'CREATE': create
 }
 
+# The main command prompt loop
 while not quitting:
+    # reset our variables
     userInput = ''
     queryDone = False
     dotCommand = False
 
     while not queryDone:
-        userInput = userInput + raw_input('> ');
+        # always append prior user input for multiline support
+        userInput += raw_input('> ');
+
+        # determine if the input is a dot-command
         dotCommand = len(userInput) > 0 and userInput[0] == '.'
 
         if dotCommand:
-            break
+            break # dot-commands aren't multiline/multi-keyword, so bail
         else:
+            # we're done collecting input if input is non-empty and last token is a ';'
             queryDone = len(userInput) > 0 and userInput[-1] == ';'
+
+            # if no ';', but input is non-empty, append a ' ' to delimit further input
             if not queryDone and len(userInput) > 0:
-                userInput = userInput + ' '
+                userInput += ' '
 
     if dotCommand:
         try:
-            dotCommands[userInput]()
+            dotCommands[userInput]() # try calling the dotCommand function keyed by input
         except KeyError:
             error = 'Error: unknown command or invalid arguments:  "'
-            error += userInput[1:-1]
+            error += userInput[1:-1] # strip off the '.'
             error += '". Enter ".help" for help'
             print(error)
     else:
         # strip all trailing whitespace and semicolon
         userInput = re.sub(r'\s*;*$', '', userInput)
+
+        # if stripping trailing whitespace and semicolon still leaves us with input
         if not userInput == '':
             try:
-                action = userInput.split(' ', 1)[0]
-                queryString = userInput.split(' ', 1)[1]
-                queryCommands[action](queryString)
+                action = userInput.split(' ', 1)[0] # grab the first keyword (action)
+                queryString = userInput.split(' ', 1)[1] # store the remaining query for parsing
+                queryCommands[action](queryString) # call the query function keyed by action
             except:
                 print('Error: near "%s": syntax error' %action)
 
